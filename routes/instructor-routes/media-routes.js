9c00db4fea8236b9f55ec41d24e2288316941542
ali husnain <husnainmcs.dev@gmail.com>
2024-12-28 19:31:34 +0500

    set media

--- routes/instructor-routes/media-routes.js
+++ routes/instructor-routes/media-routes.js
@@ -11,6 +11,12 @@ const upload = multer({ dest: "uploads/" });
 
 router.post("/upload", upload.single("file"), async (req, res) => {
   try {
+    if (!req.file) {
+      return res
+        .status(400)
+        .json({ success: false, message: "No file uploaded" });
+    }
+    // Directly upload buffer to Cloudinary
     const result = await uploadMediaToCloudinary(req.file.path);
     res.status(200).json({
       success: true,
